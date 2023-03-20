a3009a5f7b000596015bb74029c6d95a9768ce56
aldofc <aldofc18@gmail.com>
2023-03-20 00:36:21 -0600

    modelos-relaciones

---
+++ api/src/models/Activity.js
@@ -0,0 +1,36 @@
+const { DataTypes } = require('sequelize');
+// Exportamos una funcion que define el modelo
+// Luego le injectamos la conexion a sequelize.
+module.exports = (sequelize) => {
+  // defino el modelo
+  sequelize.define('Activity', {
+
+    id: {
+      type: DataTypes.UUID,
+      defaultValue: DataTypes.UUIDV4,
+      primaryKey: true
+    },
+
+    name: {
+      type: DataTypes.STRING,
+      allowNull: false,
+    },
+
+    difficulty: {
+      type: DataTypes.INTEGER,
+      allowNull: false,
+    },
+
+    duration: {
+      type: DataTypes.INTEGER,
+      
+    },
+
+    season: {
+      type: DataTypes.STRING,
+      allowNull: false,
+    }
+  }, {
+    timestamps:false
+  });
+};
---
+++ api/src/models/Country.js
@@ -0,0 +1,49 @@
+const { DataTypes } = require('sequelize');
+// Exportamos una funcion que define el modelo
+// Luego le injectamos la conexion a sequelize.
+module.exports = (sequelize) => {
+  // defino el modelo
+  sequelize.define('Country', {
+    id: {
+      type: DataTypes.UUID,
+      defaultValue: DataTypes.UUIDV4,
+      primaryKey: true
+    },
+
+    name: {
+      type: DataTypes.STRING,
+      allowNull: false,
+    },
+
+    image: {
+      type: DataTypes.STRING,
+      allowNull: false,
+    },
+
+    continent: {
+      type: DataTypes.STRING,
+      allowNull: false,
+    },
+
+    capital: {
+      type: DataTypes.STRING,
+      allowNull: false,
+    },
+
+    subregion: {
+      type: DataTypes.STRING,  
+    },
+
+    area: {
+      type: DataTypes.INTEGER,
+    },
+
+    population: {
+      type: DataTypes.INTEGER
+    }
+  },
+  {
+        timestamps:false
+    });
+  };
+  
